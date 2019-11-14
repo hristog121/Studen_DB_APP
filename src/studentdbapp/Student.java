58a1ac351be6bd61f23ce2eee8415ba34b26a60a
hristog121 <hristog121@kth.se>
2019-11-14 22:33:06 +0100

    student-object and initial prompts done

--- src/studentdbapp/Student.java
+++ src/studentdbapp/Student.java
@@ -1,4 +1,49 @@
 package studentdbapp;
 
+import java.util.Scanner;
+
 public class Student {
+
+    private String firstName;
+    private String lastName;
+    private String lastYear;
+    private int gradeYear;
+    private String studentID;
+    private String copurses;
+    private int tuitionBalance;
+    private static int costOfCourse = 600;
+    private static int id = 1000;
+
+
+    // Constructor: ask user to enter students name and year
+    public Student() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter Student First Name: ");
+        this.firstName = in.nextLine();
+
+        System.out.println("Enter Student Last Name: ");
+        this.lastName = in.nextLine();
+
+        System.out.println("1 - Freshmen\n2 - Softmore\n3 - Junior\n4 - Senior Enter Student Grade Level: ");
+        this.gradeYear = in.nextInt();
+
+        setStudentID();
+        System.out.println(firstName + " " + lastName + " " + gradeYear + " " + studentID);
+
+
+
+    }
+    // Generate ID
+    private void  setStudentID(){
+        // Grade Level + ID
+        id++;
+        this.studentID = gradeYear + "" + id;
+
+    }
+    // Enroll in courses
+
+    // View balance and pay tuition
+
+    // Show status
+
 }
--- src/studentdbapp/StudentDbApp.java
+++ src/studentdbapp/StudentDbApp.java
@@ -3,5 +3,11 @@ package studentdbapp;
 public class StudentDbApp {
     public static void main(String[] args) {
 
+        Student stu1 = new Student();
+
+        //Ask how many new users we want to add
+
+
+        // Create n number of new students
     }
 }
