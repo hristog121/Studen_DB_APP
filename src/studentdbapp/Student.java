d8dd48cb34dca015ede9ad9a90a33442787304ad
hristog121 <hristog121@kth.se>
2019-11-15 23:29:46 +0100

    everything done before array

--- src/studentdbapp/Student.java
+++ src/studentdbapp/Student.java
@@ -9,8 +9,8 @@ public class Student {
     private String lastYear;
     private int gradeYear;
     private String studentID;
-    private String copurses;
-    private int tuitionBalance;
+    private String courses = null;
+    private int tuitionBalance = 0;
     private static int costOfCourse = 600;
     private static int id = 1000;
 
@@ -40,10 +40,47 @@ public class Student {
         this.studentID = gradeYear + "" + id;
 
     }
+
     // Enroll in courses
+    public void enroll(){
+        // Get inside loop, user hits 0
+        do {
+            System.out.println("Enter course to enroll (Q to quit): ");
+            Scanner in = new Scanner(System.in);
+            String course = in.nextLine();
+            if (!course.equals("Q")){
+                courses = courses + "\n " + course;
+                tuitionBalance = tuitionBalance + costOfCourse;
+            } else {
+                break;
+            }
+        }while (1 != 0);
+
+        System.out.println("Enrolled in: " + courses);
+    }
 
     // View balance and pay tuition
+    public void viewBalance(){
+        System.out.println("Your balance is: " + tuitionBalance + " €");
+    }
+
+    // Pay tuition
+    public void payTuition(){
+        viewBalance();
+        System.out.println("Please enter your payment: ");
+        Scanner in = new Scanner(System.in);
+        int payment = in.nextInt();
+        tuitionBalance = tuitionBalance - payment;
+        System.out.println("Thank you for your payment of " + payment +" €");
+        viewBalance();
+    }
 
     // Show status
-
+    public String showInfo(){
+        return "Name: " + firstName + " " + lastName +
+                "\nGrade level: " + gradeYear +
+                "\nSudent ID: " + studentID +
+                "\nCourses Enrolled: " + courses +
+                "\nBalance: " + tuitionBalance + " €";
+    }
 }
--- src/studentdbapp/StudentDbApp.java
+++ src/studentdbapp/StudentDbApp.java
@@ -4,6 +4,9 @@ public class StudentDbApp {
     public static void main(String[] args) {
 
         Student stu1 = new Student();
+        stu1.enroll();
+        stu1.payTuition();
+        System.out.println(stu1.showInfo());
 
         //Ask how many new users we want to add
 
