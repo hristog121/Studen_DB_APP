7ffae9d8f59de01fd0d5960679a7b5e8ca46816b
hristog121 <hristog121@kth.se>
2019-11-16 11:03:07 +0100

    Final Version

--- src/studentdbapp/StudentDbApp.java
+++ src/studentdbapp/StudentDbApp.java
@@ -1,16 +1,24 @@
 package studentdbapp;
 
+import java.util.Scanner;
+
 public class StudentDbApp {
     public static void main(String[] args) {
 
-        Student stu1 = new Student();
-        stu1.enroll();
-        stu1.payTuition();
-        System.out.println(stu1.showInfo());
-
-        //Ask how many new users we want to add
+        //Ask how many new students we want to add
+        System.out.println("Enter number of new students to enroll: ");
+        Scanner in = new Scanner(System.in);
+        int numberOfStudents = in.nextInt();
+        Student [] students = new Student[numberOfStudents];
 
 
         // Create n number of new students
+        for (int i = 0; i < numberOfStudents; i++) {
+            students[i] = new Student();
+            students[i].enroll();
+            students[i].payTuition();
+            System.out.println(students[i].showInfo());
+            System.out.println(students[i].toString());
+        }
     }
 }
